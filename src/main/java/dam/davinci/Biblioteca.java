e18211d503965a8d6bcf5ba2f53db53d2af0ad4f
rsgp229-dam-davinci <rsgp229.elastic262@passmail.net>
2024-05-26 20:37:08 +0200

    Añadiendo método 'guardarEnBaseDeDatos' a la clase 'Biblioteca'. Añadiendo archivo de propiedades para la conexión a la base de datos.

--- src/main/java/dam/davinci/Biblioteca.java
+++ src/main/java/dam/davinci/Biblioteca.java
@@ -1,7 +1,14 @@
 package dam.davinci;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class Biblioteca {
     private List<Libro> libros;
@@ -38,5 +45,30 @@ public class Biblioteca {
             }
         }
         return null;
+    }
+
+    private Connection getConnection() throws IOException, SQLException {
+        Properties properties = new Properties();
+        FileReader reader = new FileReader("database.properties");
+        properties.load(reader);
+        return DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password"));
+    }
+
+    public boolean guardarEnBaseDeDatos(Libro libro) {
+        String statement = "INSERT INTO persons (author, title) values (?,?)";
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
+            preparedStatement.setString(1,libro.getAuthor());
+            preparedStatement.setString(2, libro.getTitle());
+            preparedStatement.execute();
+            return true;
+        } catch (Exception e) {
+            System.err.println("Ha ocurrido algún problema durante la inserción en la base de datos");
+            System.err.println(e.getMessage());
+        }
+        return false;
     }
 }
